b326d9055129b39c8bbf682f0872b81fb7a8a4b0
Fiolancs <171961149+Fiolancs@users.noreply.github.com>
2024-12-30 09:23:28 +0900

    add threadpool

---
+++ OFS-lib/OFS_ThreadPool.cpp
@@ -0,0 +1,66 @@
+#include "OFS_ThreadPool.h"
+
+//#include "BS_thread_pool.hpp"
+#include "../3rdParty/thread-pool/include/BS_thread_pool.hpp"
+
+#include <memory>
+#include <string>
+#include <utility>
+
+namespace
+{
+#ifdef NDEBUG
+    using PoolImpl_t = BS::light_thread_pool;
+#else
+    using PoolImpl_t = BS::wdc_thread_pool;
+#endif
+
+    PoolImpl_t& getPoolImpl(std::shared_ptr<void>& impl) noexcept
+    {
+        return *std::static_pointer_cast<PoolImpl_t>(impl);
+    }
+}
+
+static std::unique_ptr<OFS::ThreadPool> gThreadPool = nullptr;
+
+OFS::ThreadPool& OFS::ThreadPool::get(void) noexcept
+{
+    static const int nonce = [] { 
+        gThreadPool = std::make_unique<OFS::ThreadPool>(std::thread::hardware_concurrency(), "OFS gThreadPool"); 
+        return 1; 
+    }();
+
+    return *gThreadPool;
+}
+
+void OFS::ThreadPool::detachTask(std::function<void(void)> f)
+{
+    auto& impl = getPoolImpl(pImpl);
+    impl.detach_task(std::move(f));
+    // Consider making a 0s wait here to clear the detached tasks instead for user simplicity?
+    //{
+    //	using namespace std::chrono_literals;
+    //	impl.wait_for(0ms);
+    //}
+}
+
+bool OFS::ThreadPool::waitDetachedTasks(std::chrono::nanoseconds ns)
+{
+    auto& impl = getPoolImpl(pImpl);
+    return impl.wait_for(ns);
+}
+
+OFS::ThreadPool::ThreadPool(unsigned poolSize, std::string_view name)
+    : pImpl{ std::make_shared<PoolImpl_t>(
+            poolSize, 
+            [name = std::string(name)] { 
+                if (!name.empty()) BS::this_thread::set_os_thread_name(name);
+            }
+    )}
+{
+}
+
+OFS::ThreadPool::~ThreadPool(void) noexcept
+{
+    getPoolImpl(pImpl).wait();
+}
---
+++ OFS-lib/OFS_ThreadPool.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include <chrono>
+#include <memory>
+#include <future>
+#include <string_view>
+
+namespace OFS
+{
+    class ThreadPool
+    {
+    public:
+        // Gets the global thread pool 
+        static ThreadPool& get(void) noexcept;
+
+        template <typename Task, typename ... Args>
+        auto queueTask(Task&& task, Args&& ...);
+        void detachTask(std::function<void(void)>);
+
+        // MUST periodically call this
+        // \returns 
+        //  true if wait finished, false if wait expired
+        bool waitDetachedTasks(std::chrono::nanoseconds);
+
+        // Create a local private thread pool
+        explicit ThreadPool(unsigned poolSize = std::thread::hardware_concurrency(), std::string_view name = {});
+        ~ThreadPool(void) noexcept;
+
+    private:
+        std::shared_ptr<void> pImpl;
+    };
+}
+
+template <typename Task, typename ...Args>
+inline auto OFS::ThreadPool::queueTask(Task&& task, Args&& ... args)
+{
+    std::packaged_task t{ [t = std::forward<Task>(task), ...args = std::forward<Args>(args)] { return t(std::forward<Args>(args)...); } };
+    auto future = t.get_future();
+
+    detachTask([f = std::move(t)] { f(); });
+
+    return future;
+}
