6c4fb9496911e9ed430a0f92642e96dd62cab548
Fiolancs <171961149+Fiolancs@users.noreply.github.com>
2025-01-01 18:45:21 +0900

    move libstate into openfunscripterstate and remove libstate

--- src/state/OpenFunscripterState.cpp
+++ src/state/OpenFunscripterState.cpp
@@ -1,30 +1,47 @@
-#include "OpenFunscripterState.h"
-#include "ScriptModeState.h"
-#include "MetadataEditorState.h"
-#include "PreferenceState.h"
-#include "SimulatorState.h"
-#include "ProjectState.h"
-#include "SpecialFunctionsState.h"
-#include "WebsocketApiState.h"
+#include "state/OpenFunscripterState.h"
+#include "state/ScriptModeState.h"
+#include "state/MetadataEditorState.h"
+#include "state/PreferenceState.h"
+#include "state/SimulatorState.h"
+#include "state/ProjectState.h"
+#include "state/SpecialFunctionsState.h"
+#include "state/WebsocketApiState.h"
+#include "state/states/VideoplayerWindowState.h"
+#include "state/states/BaseOverlayState.h"
+#include "state/states/WaveformState.h"
+#include "state/states/KeybindingState.h"
+#include "state/states/ChapterState.h"
 
 #include "state/OFS_StateManager.h"
+#include "state/OFS_StateHandle.h"
 
 
 void OpenFunscripterState::RegisterAll() noexcept
 {
+	// QQQ
+	// 
+	// This is here so that the states are actually registered 
+	// before the app attempts to deserialize them then register them again
+	// kiv to see if we should do something about it
+	
 	// App state
-	OFS_REGISTER_STATE(OpenFunscripterState);
-	OFS_REGISTER_STATE(ScriptingModeState);
-	OFS_REGISTER_STATE(FunscriptMetadataState);
-	OFS_REGISTER_STATE(PreferenceState);
-	OFS_REGISTER_STATE(SimulatorDefaultConfigState);
-	OFS_REGISTER_STATE(SpecialFunctionState);
-	OFS_REGISTER_STATE(WebsocketApiState);
-
+	OFS::AppState<OpenFunscripterState>::registerState(OpenFunscripterState::StateName, OpenFunscripterState::StateName);
+	OFS::AppState<ScriptingModeState>::registerState(ScriptingModeState::StateName, ScriptingModeState::StateName);
+	OFS::AppState<FunscriptMetadataState>::registerState(FunscriptMetadataState::StateName, FunscriptMetadataState::StateName);
+	OFS::AppState<PreferenceState>::registerState(PreferenceState::StateName, PreferenceState::StateName);
+	OFS::AppState<SimulatorDefaultConfigState>::registerState(SimulatorDefaultConfigState::StateName, SimulatorDefaultConfigState::StateName);
+	OFS::AppState<SpecialFunctionState>::registerState(SpecialFunctionState::StateName, SpecialFunctionState::StateName);
+	OFS::AppState<WebsocketApiState>::registerState(WebsocketApiState::StateName, WebsocketApiState::StateName);
+	OFS::AppState<OFS_KeybindingState>::registerState(OFS_KeybindingState::StateName, OFS_KeybindingState::StateName);
+	OFS::AppState<BaseOverlayState>::registerState(BaseOverlayState::StateName, BaseOverlayState::StateName);
+	
 	// Project state
-	OFS_REGISTER_STATE(TempoOverlayState);
-	OFS_REGISTER_STATE(ProjectState);
-	OFS_REGISTER_STATE(SimulatorState);
+	OFS::ProjectState<TempoOverlayState>::registerState(TempoOverlayState::StateName, TempoOverlayState::StateName);
+	OFS::ProjectState<ProjectState>::registerState(ProjectState::StateName, ProjectState::StateName);
+	OFS::ProjectState<SimulatorState>::registerState(SimulatorState::StateName, SimulatorState::StateName);
+	OFS::ProjectState<VideoPlayerWindowState>::registerState(VideoPlayerWindowState::StateName, VideoPlayerWindowState::StateName);
+	OFS::ProjectState<WaveformState>::registerState(WaveformState::StateName, WaveformState::StateName);
+	OFS::ProjectState<ChapterState>::registerState(ChapterState::StateName, ChapterState::StateName);
 }
 
 void OpenFunscripterState::addRecentFile(const RecentFile& recentFile) noexcept
